16c0835b0d117b6b2cd83bc847d25bfb5c4dbf29
TupleDev <107370797+TupleDev@users.noreply.github.com>
2022-06-15 17:28:08 +0100

    Update eac.h

--- eac.h
+++ eac.h
@@ -5,7 +5,7 @@ namespace eac
 
 	auto hk_allocation( INT64 type, size_t size, uintptr_t tag ) -> __int64 __fastcall
 	{
-		if ( size == 33096 + 16 )
+		if ( size == 33096 + 16 ) // 16 size of a packet
 		{
 			DbgPrintEx( 0, 0, "EasyAntiCheat Hydra packet denied\n" );
 			return 0;
@@ -16,24 +16,9 @@ namespace eac
 
 	auto hook_allocation( const uintptr_t base, const size_t size ) -> bool
 	{
-		if ( size <= 0x1000 )
-		{
-			return false;
-		}
-
 		const auto *dos = reinterpret_cast< IMAGE_DOS_HEADER * >( base );
 
-		if ( dos->e_magic != IMAGE_DOS_SIGNATURE )
-		{
-			return false;
-		}
-
 		const auto *nt = reinterpret_cast< IMAGE_NT_HEADERS * >( base + dos->e_lfanew );
-
-		if ( nt->Signature != IMAGE_NT_SIGNATURE )
-		{
-			return false;
-		}
 
 		ex_allocate_pool_with_tag_fn = *reinterpret_cast< ex_allocate_pool_with_tag_t* >( base + 0xD7B80 );
 		*reinterpret_cast<uintptr_t*>( base + 0xD7B80 ) = reinterpret_cast<uintptr_t>( &hk_allocation );
